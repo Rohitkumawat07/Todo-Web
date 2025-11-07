5613be84360ca9f4ffa4c72d8590dc902d0427ed
Rohit kumar kumawat <rohitkumawatrk2024@gmail.com>
2025-11-07 19:29:35 +0530

    Set base path for deployment in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Todo-Web/', // ⚠️ <-- yahan apne GitHub repo ka exact naam likh (case sensitive)
   server: {
     port: 3000,
     open: true
